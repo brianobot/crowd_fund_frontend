12a205a5d56cfd37c926932490a8f15137165de5
Brian Obot <brianobot@gmail.com>
2024-11-26 16:19:37 +0100

    Initial Commit 🍃

--- src/views/basics/index.tsx
+++ src/views/basics/index.tsx
@@ -1,5 +1,6 @@
 
 import { FC } from "react";
+import { Campaign } from '../../components/Campaign';
 import { SignMessage } from '../../components/SignMessage';
 import { SendTransaction } from '../../components/SendTransaction';
 import { SendVersionedTransaction } from '../../components/SendVersionedTransaction';
@@ -10,15 +11,13 @@ export const BasicsView: FC = ({ }) => {
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8">
-          Basics
+          Crowd Fund Basic Actions
         </h1>
         {/* CONTENT GOES HERE */}
         <div className="text-center">
-          <SignMessage />
-          <SendTransaction />
-          <SendVersionedTransaction />
+          <Campaign />
         </div>
       </div>
     </div>
-  );
+  ); 
 };
